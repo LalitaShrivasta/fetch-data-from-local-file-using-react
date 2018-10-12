c8b91b30fbc6ce7a3f7e306c7a1dd9f5cddd96b3
LalitaShrivasta <lalita17@navgurukul.org>
2018-10-12 11:45:15 +0530

    Updates

---
+++ precache-manifest.58408e99a23ab8edbae3796eda0b2901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fetch-data-using-react-js/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3f14a3bce32b2658e574",
+    "url": "/fetch-data-using-react-js/static/js/runtime~main.3f14a3bc.js"
+  },
+  {
+    "revision": "105531af51d798027f48",
+    "url": "/fetch-data-using-react-js/static/js/main.105531af.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/fetch-data-using-react-js/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "105531af51d798027f48",
+    "url": "/fetch-data-using-react-js/static/css/main.c8bb8910.chunk.css"
+  },
+  {
+    "revision": "22b5dd80b196561e6dcf575b7b9386ca",
+    "url": "/fetch-data-using-react-js/index.html"
+  }
+];
